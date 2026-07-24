644d34864986d54d9adffb4b94cf59cd311599d7
lucifermornngstar52-cell <lucifermornngstar52@gmail.com>
2026-07-24 11:50:00 +0500

    fix: set minSdk=24 for flutter_tts compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         applicationId = "com.airi.assistant"
-        minSdk = flutter.minSdkVersion
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
